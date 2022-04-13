498dd406ba9d83674de9705a789fbc196ca22026
邱珑 <qiulong@corp.netease.com>
2022-04-13 10:51:53 +0800

    test2

---
+++ app/src/main/java/com/example/plus2/demos/test2.java
@@ -0,0 +1,12 @@
+package com.example.plus2.demos;
+
+/**
+ * @title:
+ * @description:
+ * @company: Netease
+ * @author: 邱珑
+ * @blame: 邱珑
+ * @version: Created on 2022/4/13.
+ */
+public class test2 {
+}
